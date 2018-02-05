c2813aa713fa7ecfe1710cde9395176c3cb1de97
Haton Arthur <arthur.haton@orange.fr>
2018-02-05 21:44:49 +0100

    Add crypto choice

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ const REFRESH_TIME = 30;
 
 let lastvalue = 0;
 
-function refresh(first = false){
-    const req = new XMLHttpRequest();
 
-    if(first){
-        getLastValue();
-    }
+function refresh(){
+    const req = new XMLHttpRequest();
 
     req.onreadystatechange = function(event) {
         // XMLHttpRequest.DONE === 4
@@ -17,10 +14,10 @@ function refresh(first = false){
                 let obj = JSON.parse(this.responseText);
                 saveInStorage(obj);
 
-                let val = parseFloat(obj.last);
+                let val = obj.last;
 
                 if(val >= 100){
-                    val = Math.floor(val) + "";
+                    val = Math.floor(parseFloat(val)) + "";
                 }
 
                 browser.browserAction.setBadgeText({
@@ -48,21 +45,38 @@ function saveInStorage(obj) {
     });
 }
 
-function getLastValue() {
-    let gettingItem = browser.storage.local.get('bt_last');
-    gettingItem.then((res) => {
-        browser.browserAction.setBadgeText({
-            text: res.bt_last,
-        });
+function updateTitle(cryptoName, timestamp, value){
+    let date = new Date(timestamp*1000);
+    let hours = date.getHours();
+    let minutes = "0" + date.getMinutes();
+    let seconds = "0" + date.getSeconds();
 
-        lastvalue = res.bt_last;
+    let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+    browser.browserAction.setTitle({
+        title: 'Etherum à ' + formattedTime + " : " + value + " €"
     });
 }
 
+/*
+Log the storage area that changed,
+then for each item changed,
+log its old value and its new value.
+*/
+function logStorageChange(changes, area) {
+    lastvalue = changes["bt_last"].newValue;
+    let crypto = "Etherum";
+    updateTitle(crypto, changes["bt_timestamp"].newValue, changes["bt_last"].newValue);
+}
 
+browser.browserAction.setTitle({
+    title: 'Etherum'
+});
 browser.browserAction.setBadgeBackgroundColor({color: "grey"});
 
-refresh(true);
+refresh();
+
+browser.storage.onChanged.addListener(logStorageChange);
 
 window.setInterval(function(){
     refresh();
--- popup/js/bitstamp.js
+++ popup/js/bitstamp.js
@@ -1,14 +1,24 @@
 // In second
 const REFRESH_TIME = 30;
 
-const lastupdateBox = document.getElementById("lastupdate");
+let lastupdateBox = document.getElementById("lastupdate");
 
-function updateContent() {
-    /*browser.storage.local.get('timestamp').then((storedInfo) => {
-        lastupdateBox.innerHTML = storedInfo['timestamp'];
-    });*/
+lastupdateBox.innerText = "fds";
 
-    lastupdateBox.value = "Test";
+function updateDate(timestamp) {
+    let date = new Date(timestamp*1000);
+    let hours = date.getHours();
+    let minutes = "0" + date.getMinutes();
+    let seconds = "0" + date.getSeconds();
+
+    let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    lastupdateBox.innerText = formattedTime;
 }
 
-updateContent();
+function updatePopup() {
+    browser.storage.local.get('bt_timestamp').then(function(item){
+        updateDate(item.bt_timestamp);
+    });
+}
+
+updatePopup();
