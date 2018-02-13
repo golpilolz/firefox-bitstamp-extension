a3f65db98004239c6f1b92e9a2eac60e2f19149a
Arthur Haton <arthur.haton@orange.fr>
2018-02-13 22:48:03 +0100

    Adds all Bitstamp currencies

--- src/cryptos.js
+++ src/cryptos.js
@@ -4,12 +4,65 @@
  */
 function getCryptos() {
     let cryptos = [];
-    cryptos['etheur'] = [];
-    cryptos['etheur']['name'] = 'ETH / EUR';
-    cryptos['etheur']['icon'] = 'icons/eth-32.png';
+    cryptos['btcusd'] = [];
+    cryptos['btcusd']['name'] = 'BTC / USD';
+    cryptos['btcusd']['icon'] = 'icons/btc-32.png';
+
+    cryptos['btceur'] = [];
+    cryptos['btceur']['name'] = 'BTC / EUR';
+    cryptos['btceur']['icon'] = 'icons/btc-32.png';
+
+    cryptos['eurusd'] = [];
+    cryptos['eurusd']['name'] = 'USD / EUR';
+    cryptos['eurusd']['icon'] = 'icons/eur-32.png';
+
     cryptos['xrpeur'] = [];
     cryptos['xrpeur']['name'] = 'XRP / EUR';
     cryptos['xrpeur']['icon'] = 'icons/xrp-32.png';
+
+    cryptos['xrpusd'] = [];
+    cryptos['xrpusd']['name'] = 'XRP / USD';
+    cryptos['xrpusd']['icon'] = 'icons/xrp-32.png';
+
+    cryptos['xrpbtc'] = [];
+    cryptos['xrpbtc']['name'] = 'XRP / BTC';
+    cryptos['xrpbtc']['icon'] = 'icons/xrp-32.png';
+
+    cryptos['ltcusd'] = [];
+    cryptos['ltcusd']['name'] = 'LTC / USD';
+    cryptos['ltcusd']['icon'] = 'icons/ltc-32.png';
+
+    cryptos['ltceur'] = [];
+    cryptos['ltceur']['name'] = 'LTC / EUR';
+    cryptos['ltceur']['icon'] = 'icons/ltc-32.png';
+
+    cryptos['ltcbtc'] = [];
+    cryptos['ltcbtc']['name'] = 'LTC / BTC';
+    cryptos['ltcbtc']['icon'] = 'icons/ltc-32.png';
+
+    cryptos['ethusd'] = [];
+    cryptos['ethusd']['name'] = 'ETH / USD';
+    cryptos['ethusd']['icon'] = 'icons/eth-32.png';
+
+    cryptos['etheur'] = [];
+    cryptos['etheur']['name'] = 'ETH / EUR';
+    cryptos['etheur']['icon'] = 'icons/eth-32.png';
+
+    cryptos['ethbtc'] = [];
+    cryptos['ethbtc']['name'] = 'ETH / BTC';
+    cryptos['ethbtc']['icon'] = 'icons/eth-32.png';
+
+    cryptos['bchusd'] = [];
+    cryptos['bchusd']['name'] = 'BCH / USD';
+    cryptos['bchusd']['icon'] = 'icons/bch-32.png';
+
+    cryptos['bcheur'] = [];
+    cryptos['bcheur']['name'] = 'BCH / EUR';
+    cryptos['bcheur']['icon'] = 'icons/bch-32.png';
+
+    cryptos['bchbtc'] = [];
+    cryptos['bchbtc']['name'] = 'BCH / BTC';
+    cryptos['bchbtc']['icon'] = 'icons/bch-32.png';
 
     return cryptos;
 }
